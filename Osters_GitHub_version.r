2250e0c4592a58d61deb9fb55c3b146dc539f7a4
Manuel S. Gonzalez Canche <msgc@upenn.edu>
2021-04-15 14:27:50 -0400

    To use:
    if (!require('devtools')) install.packages('devtools'); library('devtools')
    source_url("https://github.com/msgc/oster_psw/blob/main/Osters_GitHub_version.r"

--- Osters_GitHub_version.r
+++ Osters_GitHub_version.r
@@ -1,7 +1,3 @@
-# To use:
-if (!require('devtools')) install.packages('devtools'); library('devtools')
-source_url("https://github.com/msgc/oster_psw/edit/main/Oster's%20GitHub%20version.r"
-
 # The dataset for this group contains treated cases and control cases: the treated cases are those who actually received job training under the NSW experiment, and the control cases include a large number of people from the PSID, a nationally representative survey of income and employment. Since the PSID is a representative sample of the whole population while the NSW experiment was a program targeted at poor workers, you should expect that the treatment group will look very different from the control group. Remember that this is exactly the problem that matching estimators are designed to correct.
 
 
